2cc382c19d4bc37cadb84c2f722eb3b8e3fc4db6
cephscope <sworm.null@gmail.com>
2018-09-20 20:39:39 -0400

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Main = () => (
+  <div>
+    <div className="frame-top"></div>
+    <div className="frame-bottom"></div>
+    <div className="frame-left"></div>
+    <div className="frame-right"></div>
+    <div className="container">
+      <div className="content">
+        <h1>Hi, I'm Charles</h1>
+        <h2>I do stuff, I do it real good. You're on my site to see the stuff. The good stuff. Mmmm, that's the stuff.</h2>
+        <h1>Bio</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor. Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit. Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
+        <h1>work</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor. Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit. Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor. Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit. Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor. Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit. Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Main;
