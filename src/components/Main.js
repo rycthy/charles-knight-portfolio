cd7fab4f077f5929ff4760eb450b93da17ec8fba
cephscope <sworm.null@gmail.com>
2018-09-24 16:30:05 -0400

    created Top component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,50 +1,46 @@
 import React from 'react';
 import Frame from './Frame';
+import Top from './Top';
 
-const Main = () => (
+export default () => (
   <div>
     <Frame />
     <div className="container">
-        <div className="top">
-          <h2>Hi, I'm Charles.</h2>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue. Proin id erat in ante & consectetur dictum sit amet non tortor.</p>
-        </div>
-        <h2>Bio</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
-            eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
-            Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
-            laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
-            Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
-            non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
-            posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
+      <Top />
+      <h2>Bio</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
+          eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
+          Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
+          laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
+          Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
+          non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
+          posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
           quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
-        <h2>work</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
-            eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
-            Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
-            laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
-            Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
-            non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
-            posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
+      <h2>work</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
+          eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
+          Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
+          laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
+          Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
+          non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
+          posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
           quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
-            eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
-            Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
-            laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
-            Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
-            non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
-            posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
+          eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
+          Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
+          laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
+          Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
+          non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
+          posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
           quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
-            eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
-            Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
-            laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
-            Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
-            non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
-            posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
+          eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
+          Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
+          laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
+          Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
+          non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
+          posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
           quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
     </div>
   </div>
 );
-
-export default Main;
---
+++ src/components/Top.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default () => (
+  <div className="top">
+    <h2>Hi, I'm Charles.</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue. Proin id erat in ante & consectetur dictum sit amet non tortor.</p>
+  </div>
+);
