b66048f280e3cb9de19dbc79bec3073f912e3bf8
cephscope <sworm.null@gmail.com>
2018-09-24 16:35:47 -0400

    added Projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default () => (
+  <div><h2>projects</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
+        eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
+        Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
+        laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
+        Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
+        non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
+        posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
+        quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
+        eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
+        Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
+        laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
+        Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
+        non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
+        posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
+        quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
+        eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
+        Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
+        laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
+        Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
+        non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
+        posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
+        quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
+  </div>
+);
