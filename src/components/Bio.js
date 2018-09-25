30f772f2718fefdf1c2c44513bbc8e955844da6d
cephscope <sworm.null@gmail.com>
2018-09-25 13:58:04 -0400

    created bio partial, styled bio component

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 export default () => (
-  <div className="container__item">
-    <div className="column">
-      <img src="./images/me.jpeg"/>
+  <div className="container__item" id="bio">
+    <div className="column" id="portrait">
+      <img src="./images/me.jpeg" />
     </div>
-    <div className="column">
+    <div className="column" id="bio-copy">
       <p>
         My passions in marketing arise from the same passions that inspire the
         fashion and luxury goods industry I wish to specialize in: the connection of
