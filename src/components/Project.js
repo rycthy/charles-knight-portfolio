8de667db6ee5cd0d08f8365b36aec7bc12d58f6a
cephscope <sworm.null@gmail.com>
2018-09-25 14:59:57 -0400

    added Project component, styled template

---
+++ src/components/Project.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default () => (
+  <div id="projects__item">
+  <div id="image-placeholder">
+  <p>image</p>
+  </div>
+  <h2>Project Title</h2>
+  <p>brief description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus magna id finibus commodo. Fusce et nulla vel odio ornare eleifend a in sapien. Nunc fringilla nisl vel dui elementum, ac luctus magna sagittis. Proin sed elementum sapien. In efficitur in est ac commodo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam feugiat rutrum elementum. Praesent pellentesque tortor lorem, rutrum accumsan justo faucibus in.</p>
+  </div>
+)
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,35 +1,15 @@
 import React from 'react';
+import Project from './Project';
 
 export default () => (
   <div>
     <div className="container__item" id="projects-header">
       <h2>Projects</h2>
     </div>
-    <div className="container__item" id="projects-list">
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
-          eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
-          Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
-          laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
-          Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
-          non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
-          posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
-        quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
-          eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
-          Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
-          laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
-          Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
-          non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
-          posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
-        quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue
-          eleifend vel, dapibus nec dui. Proin id erat in ante consectetur dictum sit amet non tortor.
-          Pellentesque eget arcu pharetra, cursus lectus id, bibendum ex. Mauris gravida, nisl et consectetur
-          laoreet, ex risus porttitor felis, et sagittis augue magna et dui. Proin ac pellentesque velit.
-          Maecenas malesuada neque sit amet ligula fringilla, sed gravida purus iaculis. Nullam dapibus mi
-          non purus molestie, vitae aliquet dolor posuere. Nulla sagittis libero a interdum venenatis. Aenean
-          posuere, lorem pulvinar tempor ultricies, dolor nulla vehicula sapien, sit amet iaculis augue sem
-        quis orci. Nulla tempor lectus ut ipsum dapibus, ac accumsan orci varius.</p>
+    <div className="container__item" id="projects">
+      <Project />
+      <Project />
+      <Project />
     </div>
   </div>
 );
