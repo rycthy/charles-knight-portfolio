24cc2e949ca117d64f8863d836d255bf429bd185
cephscope <sworm.null@gmail.com>
2018-10-03 16:31:42 -0400

    added work section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,7 @@ import Project from './Project';
 export default () => (
   <div>
     <div className="container__item" id="projects-header">
-      <h2>Projects</h2>
+      <h2>Work</h2>
     </div>
     <div className="container__item" id="projects">
       <Project
@@ -14,9 +14,12 @@ export default () => (
       />
       <Project
         image={"https://res.cloudinary.com/dnqc6pmtp/image/upload/v1538596428/charles-knight/isaia-santoni-header.png"}
-        title={"Isaia & Santoni"}
+        title={"Valentines?"}
         description={"brief description: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris maximus magna id finibus commodo. Fusce et nulla vel odio ornare eleifend a in sapien. Nunc fringilla nisl vel dui elementum, ac luctus magna sagittis. Proin sed elementum sapien. In efficitur in est ac commodo. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam feugiat rutrum elementum. Praesent pellentesque tortor lorem, rutrum accumsan justo faucibus in."}
       />
+      <div className="container__item" id="projects-header">
+      <h2>Projects</h2>
+    </div>
       <Project
         image={"https://res.cloudinary.com/dnqc6pmtp/image/upload/v1538596428/charles-knight/isaia-santoni-header.png"}
         title={"Isaia & Santoni"}
