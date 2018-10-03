a4ce9f9b12d91a328ac9df3443ca8187c415125a
cephscope <sworm.null@gmail.com>
2018-10-03 17:40:39 -0400

    added Top copy, links, and styled links

--- src/components/Top.js
+++ src/components/Top.js
@@ -3,6 +3,11 @@ import React from 'react';
 export default () => (
   <div className="container__item top">
     <h2>Hi, I'm Charles.</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nulla risus, scelerisque congue. Proin id erat in ante & consectetur dictum sit amet non tortor.</p>
+    <p>
+      Fashion Marketer studying Fashion Brand Management at Polimoda. 
+      You can find me on <a target="_blank" href="https://www.instagram.com/driveallknight/">Instagram</a>
+      , <a target="_blank" href="https://www.linkedin.com/in/charles-knight-385a505b/">LinkedIn</a>{" "}
+      & <a target="_blank" href="mailto:knight.chas@gmail.com">email</a>
+    </p>
   </div>
 );
