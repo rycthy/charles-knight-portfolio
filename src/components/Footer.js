5d6179d58b63e62ae86421f3197ecfe3fc33fb1c
cephscope <sworm.null@gmail.com>
2018-10-03 18:08:37 -0400

    added footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ export default () => (
   <div className="footer">
     <h2>Contact</h2>
     <br />
-    <p>Email</p>
+    <p><a target="_blank" href="https://www.instagram.com/driveallknight/">Instagram</a></p>
     <br />
-    <p>LinkedIn</p>
+    <p><a target="_blank" href="https://www.linkedin.com/in/charles-knight-385a505b/">LinkedIn</a></p>
     <br />
-    <p>Instagram</p>
+    <p><a target="_blank" href="mailto:knight.chas@gmail.com">email</a></p>
   </div>
 );
